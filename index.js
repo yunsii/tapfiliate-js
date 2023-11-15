0a3d574044270d380502a94d4d29dd0379d89492
Yuns Xie <yuns.xie@qq.com>
2023-11-15 08:35:12 +0000

    refactor: tap instance

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const handler = {
-  get: function (target, name) {
-    return target.hasOwnProperty(name)
-      ? target[name]
-      : (...args) => {
-          window.__tap(name, ...args);
-        };
-  }
-};
-
-const target = {
+const Tap = {
   init: (
     accountId,
     createOptions = { integration: "npm-module" },
@@ -16,7 +6,7 @@ const target = {
     detectOptions = {},
     detectCallback
   ) => {
-    if (window.__tap) return;
+    if (window.tap) return;
 
     (function (t, a, p) {
       t.TapfiliateObject = a;
@@ -25,22 +15,22 @@ const target = {
         function () {
           (t[a].q = t[a].q || []).push(arguments);
         };
-    })(window, "__tap");
+    })(window, "tap");
 
+    const scriptSrc = "https://script.tapfiliate.com/tapfiliate.js";
     var script = document.createElement("script");
-    script.src = "https://script.tapfiliate.com/tapfiliate.js";
+    script.src = scriptSrc;
     script.type = "text/javascript";
     script.async = true;
 
     document.getElementsByTagName("head")[0].appendChild(script);
     script.addEventListener("error", () => {
-      new Error(`${this.src} failed to load.`);
+      new Error(`${scriptSrc} failed to load.`);
     });
 
-    window.__tap("create", accountId, createOptions, createCallback);
-    window.__tap("detect", detectOptions, detectCallback);
+    window.tap("create", accountId, createOptions, createCallback);
+    window.tap("detect", detectOptions, detectCallback);
   }
 };
-var Tap = new Proxy(target, handler);
 
 export default Tap;
